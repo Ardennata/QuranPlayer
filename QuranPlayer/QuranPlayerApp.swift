dbbfb96ddf0c22f30587f25845011968c22b4edd
Ardennata <ardennatawinarno@gmail.com>
2026-05-29 16:41:35 +0700

    Initial Commit

---
+++ QuranPlayer/QuranPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranPlayerApp.swift
+//  QuranPlayer
+//
+//  Created by Ardennata Winarno on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct QuranPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
